26420b88276ea7dce490a04c1c65b2c76abfb025
Dreyc Hawking <Dreycoon@gmail.com>
2018-06-24 20:12:46 -0700

    app uses database

--- kanji-flash-cards.py
+++ kanji-flash-cards.py
@@ -2,52 +2,14 @@ import sys
 import random
 from PyQt5 import QtWidgets
 from PyQt5.uic import loadUi
-
-class Kanji():
-    value = ''
-    onyomi = ''
-    kunyomi = ''
-
-    next = None
-    prev = None
-
-    def __init__(self, val, onyomi, kunyomi):
-        self.value = val
-        self.onyomi = onyomi
-        self.kunyomi = kunyomi
+import sqlite3
 
 class KanjiFlashCards(QtWidgets.QMainWindow):
-    testKanji = {
-        '一': 'ichi'
-        ,'二': 'ni'
-        ,'三': 'san'
-        ,'四': 'shi'
-        ,'五': 'go'
-        ,'六': 'roku'
-        ,'七': 'nana'
-        ,'八': 'hachi'
-        ,'九': 'kyuu'
-        ,'十': 'jyuu'
-    }
-    kanjiPtr = None
+    prevKanjiStack = []
     
     def __init__(self):
         super(KanjiFlashCards, self).__init__()
         loadUi('kanji-flash-cards.ui', self)
-
-        #shuffle kanji array and create linked list for prev/next traversal
-        randomKeys = list(self.testKanji.keys())
-        random.shuffle(randomKeys)
-        nextKanji = None
-        prevKanji = Kanji(randomKeys[0], '', self.testKanji[randomKeys[0]])
-        randomKeys.pop(0)
-        self.kanjiPtr = prevKanji
-        
-        for kanji in randomKeys:
-            nextKanji = Kanji(kanji, '', self.testKanji[kanji])
-            nextKanji.prev = prevKanji
-            prevKanji.next = nextKanji
-            prevKanji = nextKanji
 
         # bind events
         self.btnNext.clicked.connect(self.onBtnNextClicked)
@@ -56,16 +18,26 @@ class KanjiFlashCards(QtWidgets.QMainWindow):
         self.show()
 
     def onBtnNextClicked(self):
-        self.lblKanji.setText(self.kanjiPtr.value)
-        
-        if(self.kanjiPtr.next is not None):
-            self.kanjiPtr = self.kanjiPtr.next
+        #select random from db
+        try:
+            db = sqlite3.connect('kanji-flash-cards.sqlite')
+            db.row_factory = sqlite3.Row
+            cursor = db.cursor()
+            cursor.execute('SELECT * FROM kanji ORDER BY RANDOM() LIMIT 1;')
+            row = cursor.fetchone()
+            self.lblKanji.setText(row['kanji'])
+            self.prevKanjiStack.append(row)
+            print(self.prevKanjiStack)
+        except sqlite3.Error as e:
+            print(e)
 
     def onBtnPrevClicked(self):
-        self.lblKanji.setText(self.kanjiPtr.value)
+        if(len(self.prevKanjiStack)):
+            self.prevKanjiStack.pop()
+            print(self.prevKanjiStack)
 
-        if(self.kanjiPtr.prev is not None):
-            self.kanjiPtr = self.kanjiPtr.prev
+        if(len(self.prevKanjiStack)):
+            self.lblKanji.setText(self.prevKanjiStack[0]['Kanji'])
 
 def run():
     app = QtWidgets.QApplication(sys.argv)
